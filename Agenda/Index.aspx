204d61c411c2bc1aa4d977b39fa3cd4896705afd
Felipe Appolonio <puiatmp@hotmail.com>
2018-11-22 14:08:04 -0200

    Agenda

---
+++ Agenda/Index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Agenda.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Calendar id="Calendario" runat="server"
+              ForeColor="Blue"
+              Height="400"
+              Width="600"
+              BorderColor="Black"
+              BorderStyle="Double"
+              ShowGridLines="True"
+              DayNameFormat="Full"
+              FirstDayOfWeek="Sunday"
+              NextMonthText="Próximo"
+              PrevMonthText="Anterior"
+              OnSelectionChanged="Calendario_SelectionChanged"
+              OnDayRender="Calendario_DayRender"/>
+            <br />
+            <asp:Label id="lblSelecao" runat="server"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Agenda/Index.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Web.UI.WebControls;
+
+namespace Agenda
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        DataSet dsFeriados = new DataSet();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dsFeriados.ReadXml(AppDomain.CurrentDomain.BaseDirectory.ToString() +  "Feriados.xml");
+        }
+
+        protected void Calendario_SelectionChanged(object sender, EventArgs e)
+        {
+            lblSelecao.Text = "Dia(s) selecionado(s): ";
+            foreach (DateTime dia in Calendario.SelectedDates)
+            {
+                lblSelecao.Text += dia.ToString("dd/MM/yyyy") + "<br>";
+            }
+        }
+
+        protected void Calendario_DayRender(object sender, System.Web.UI.WebControls.DayRenderEventArgs e)
+        {
+            string dia;
+            DateTime feriado;
+            Style estilo = new Style();
+
+            estilo.ForeColor = System.Drawing.Color.White;
+            estilo.BackColor = System.Drawing.Color.Blue;
+
+            foreach (DataRow linha in dsFeriados.Tables[0].Rows)
+            {
+                dia = linha["Data"].ToString();
+                feriado = DateTime.Parse(dia);
+
+                if (e.Day.Date == feriado || e.Day.IsOtherMonth)
+                {
+                    e.Cell.ApplyStyle(estilo);
+                }
+            }
+        }
+    }
+}
